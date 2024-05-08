65f7c9f95d1f6fc5d88c5e53f381a616db4bc2d2
ayushschaudhari1904@gmail.com <ayushschaudhari1904@gmail.com>
2024-05-08 22:31:02 +0530

    Create Katex component

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,9 @@
 import { defineConfig } from 'astro/config';
-
 import alpinejs from "@astrojs/alpinejs";
+
+import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [alpinejs()]
+  integrations: [alpinejs(), mdx()]
 });
